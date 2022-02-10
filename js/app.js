80f7a7eed8df5bab6a426ecb649fe4500771225d
SadeqSp <sadeq.sp@gmail.com>
2022-02-10 19:28:23 +0330

    js-function for animating texts

--- js/app.js
+++ js/app.js
@@ -5,30 +5,28 @@ $('#toggle').click(function() {
 });
 
 
-// INDEX > animate
-var fbox = document.getElementById("first");
-var ftext = "MILLENNIUM CHEMICAL";
-for (let i = 0, d = 0.1; i < ftext.length; i++) {
-    let span = document.createElement("SAPN");
-    span.classList.add("animate__animated", "animate__fadeIn");
-    span.style.animationDelay = d+'s';
-    d += 0.05;
-    let c = document.createTextNode(ftext.charAt(i));
-    span.appendChild(c);
-    fbox.appendChild(span);
+// animate > fadeIn each char 
+function fadeIn_each(boxName, text, class1, class2, delay, next) {
+    var box = document.getElementById(boxName);
+    for (let i = 0, d = delay; i < text.length; i++) {
+        let span = document.createElement("SAPN");
+        span.classList.add(class1, class2);
+        span.style.animationDelay = d+'s';
+        d += next;
+        let c = document.createTextNode(text.charAt(i));
+        span.appendChild(c);
+        box.appendChild(span);
+    }
 }
 
-var sbox = document.getElementById("second");
-var stext = "EXPANDING THE HORIZON";
-for (let i = 0, d = 0.5; i < stext.length; i++) {
-    let span = document.createElement("SAPN");
-    span.classList.add("animate__animated", "animate__fadeIn");
-    span.style.animationDelay = d+'s';
-    d += 0.05;
-    let c = document.createTextNode(stext.charAt(i));
-    span.appendChild(c);
-    sbox.appendChild(span);
-}
+let index_ftext = "MILLENNIUM CHEMICAL";
+let index_stext = "EXPANDING THE HORIZON";
+
+let class1 = "animate__animated";
+let class2 = "animate__fadeIn";
+
+fadeIn_each("index_first", index_ftext, class1, class2, 1, 0.05);
+fadeIn_each("index_second", index_stext, class1, class2, 1.5, 0.05);
 
 
 //
