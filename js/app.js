1722b67df74ee8e3b90010e75663e96ff1b4ebf9
SadeqSp <sadeq.sp@gmail.com>
2022-02-13 19:42:08 +0330

    wow animate

--- js/app.js
+++ js/app.js
@@ -41,7 +41,7 @@ let page = html.replace(".html", "");
 
 
 // animate > fadeIn each char 
-function fadeIn_each(boxName, text, class1, class2, delay, next, orange=0) {
+function fadeIn_each(boxName, text, class1, class2, delay, next) {
     var box = document.getElementById(boxName);
     for (let i = 0, d = delay; i < text.length; i++) {
         let span = document.createElement("SAPN");
@@ -66,15 +66,17 @@ let csr_text = "CORPORATE SOCIAL RESPONSIBILITY";
 let con_text = "GET IN TOUCH WITH US";
 let abt_text = "WHO WE ARE";
 
-let class1 = "animate__animated";
-let class2 = "animate__fadeIn";
+// let class1 = "animate__animated";
+// let class2 = "animate__fadeIn";
+let class1 = "wow";
+let class2 = "fadeIn";
 
 if(page == 'index') {
     fadeIn_each("index_second", index_stext, class1, class2, 1.5, 0.05);
     fadeIn_each("index_first", index_ftext, class1, class2, 1, 0.05);
 }
 if(page == 'csr') {
-    fadeIn_each("csr_first", csr_text, class1, class2, 0.5, 0.05, 'yes');
+    fadeIn_each("csr_first", csr_text, class1, class2, 0.5, 0.05);
 }
 if(page == 'contact') {
     fadeIn_each("con_first", con_text, class1, class2, 0.5, 0.05);
@@ -100,3 +102,43 @@ function reveal() {
     }
 }
 window.addEventListener("scroll", reveal);
+
+
+
+
+/*
+The MIT License (MIT)
+
+Copyright (c) 2016 Thomas Grainger.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+
+Portions of this wow.js Software may utilize the following copyrighted material, the use of which is hereby acknowledged.
+
+WOW: https://github.com/matthieua/WOW/tree/20848c410fe32d161c2330e1d261b59512094f81
+
+Copyright (C) 2014; Matthieu Aussaguel
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
